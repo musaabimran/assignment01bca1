abbb712515037fb12d804a607f6709081ae9356c
Musaab Imran <96174078+musaabimran@users.noreply.github.com>
2023-09-15 14:25:57 -0700

    genesis blocl

--- assignment01bca.go
+++ assignment01bca.go
@@ -30,15 +30,22 @@ func CalculateHash(stringToHash string) string {
 // The NewBlock() method will create a new block and assign it the values and return the block
 // Previous Block parameter was added so that we can keep track of the previous block
 // making a chain of blocks (link list)
-func NewBlock(transaction string, nonce int, previousHash string, previousBlock *block) *block {
+func NewBlock(transaction string, nonce int, previousBlock *block) *block {
 	b := new(block)
 	b.transaction = transaction
 	b.nonce = nonce
-	b.previousHash = previousHash
+	if previousBlock != nil {
+		// Concatenating the transaction, nonce and previous hash to create a string for hash calculation
+		b.previousHash = previousBlock.currentHash
+		stringToHash := fmt.Sprintf("%s%d%s", transaction, nonce, previousBlock.currentHash)
+		b.currentHash = CalculateHash(stringToHash)
+	} else {
+		// Concatenating the transaction, nonce and previous hash to create a string for hash calculation for Genesis Block
+		b.previousHash = ""
+		stringToHash := fmt.Sprintf("%s%d%s", transaction, nonce, "")
+		b.currentHash = CalculateHash(stringToHash)
+	}
 
-	// Concatenating the transaction, nonce and previous hash to create a string for hash calculation
-	stringToHash := fmt.Sprintf("%s%d%s", transaction, nonce, previousHash)
-	b.currentHash = CalculateHash(stringToHash)
 	b.previousBlock = previousBlock
 	return b
 }
